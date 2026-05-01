daddd83d4ec520032af492da0e0d61184b827027
Jerry Yang <yuquany1@uci.edu>
2026-05-01 15:18:52 -0700

    analytics draft

--- scraper.py
+++ scraper.py
@@ -1,8 +1,7 @@
-import re
-from urllib.parse import urlparse
-
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from validator import is_valid
+from analytics import *
 
 MAX_PAGES = 5   # change this for testing
 visited_count = 0
@@ -14,6 +13,10 @@ def scraper(url, resp):
         return []   # stop expanding frontier
     visited_count += 1
     # TODO: delete until here
+
+    if not is_valid_response(resp):
+        return list()
+    
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
@@ -27,7 +30,7 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    if (resp.status != 200 or resp.raw_response is None):
+    if not is_valid_response(resp):
         return list()
     
     result_links = []
@@ -41,24 +44,6 @@ def extract_next_links(url, resp):
         result_links.append(absolute_url)
     return result_links
 
-def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
-    try:
-        parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
-            return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
-    except TypeError:
-        print ("TypeError for ", parsed)
-        raise
+def is_valid_response(resp):
+    return resp.status == 200 and resp.raw_response is not None
