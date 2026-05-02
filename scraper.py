d82e47e830da656e1084952d4bd76daec166e614
Amy Zhou <yingz71@uci.edu>
2026-05-01 17:08:24 -0700

    update validator and connect scraper

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from validator import is_valid
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -17,24 +18,13 @@ def extract_next_links(url, resp):
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     return list()
 
-def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
-    try:
-        parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
-            return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+if __name__ == "__main__":
+    test_urls = [
+        "https://www.ics.uci.edu",
+        "https://www.google.com",
+        "https://www.ics.uci.edu/test.pdf",
+        "https://www.ics.uci.edu/a/a/a/a/a/a/a/a/a/a/a"
+    ]
 
-    except TypeError:
-        print ("TypeError for ", parsed)
-        raise
+    for url in test_urls:
+        print(url, is_valid(url))
